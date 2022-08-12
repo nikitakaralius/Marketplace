bb4b7f2f573d9a914f3266642b060d2a3c69d0b6
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-12 13:53:56 +0300

    Registered db services

--- src/Marketplace/Startup/DependencyInjection.cs
+++ src/Marketplace/Startup/DependencyInjection.cs
@@ -2,18 +2,29 @@ using Marketplace.Domain.Services;
 using Marketplace.Framework;
 using Marketplace.Infrastructure;
 using Marketplace.Infrastructure.EntityFramework;
+using Microsoft.EntityFrameworkCore;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
 
 internal static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddControllers();
+        services.AddEntityFrameworkNpgsql()
+                .AddDbContext<ClassifiedAdDbContext>(options =>
+                {
+                    string connectionString = configuration.GetConnectionString("Postgres");
+                    options.UseNpgsql(connectionString);
+                });
+
         services.AddSingleton<ICurrencyLookup, FixedCurrencyLookup>();
+
         services.AddScoped<IUnitOfWork, EfCoreUnitOfWork>();
+        services.AddScoped<IClassifiedAdRepository, ClassifiedAdRepository>();
         services.AddScoped<ClassifiedAdsApplicationService>();
+
+        services.AddControllers();
         services.AddSwaggerGen(c =>
         {
             c.SwaggerDoc("v1",
@@ -23,6 +34,7 @@ internal static class DependencyInjection
                              Version = "v1"
                          });
         });
+
         return services;
     }
 }
