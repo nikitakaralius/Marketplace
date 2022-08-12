c95b1d22d8676233c7eb74faa19d0c251ea915b4
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-12 19:22:41 +0300

    Added method actions to UserProfile

--- src/Marketplace.Domain/UserProfile/UserProfile.cs
+++ src/Marketplace.Domain/UserProfile/UserProfile.cs
@@ -4,8 +4,14 @@ using static Marketplace.Domain.UserProfile.Events;
 
 namespace Marketplace.Domain.UserProfile;
 
+using UserEvent = IEvent<UserProfile>;
+
 public sealed class UserProfile : AggregateRoot
 {
+    private UserProfile()
+    {
+    }
+
     public Guid DatabaseId { get; private set; }
 
     public UserId Id { get; private set; } = null!;
@@ -24,9 +30,38 @@ public sealed class UserProfile : AggregateRoot
             DisplayName = displayName
         });
 
+    public void UpdateFullName(FullName fullName) =>
+        Apply(new UserFullNameUpdated
+        {
+            UserId = Id,
+            FullName = fullName
+        });
+
+    public void UpdateDisplayName(DisplayName displayName) =>
+        Apply(new UserDisplayNameUpdated
+        {
+            UserId = Id,
+            DisplayName = displayName
+        });
+
+    public void UpdateProfilePhoto(Uri photoUrl) =>
+        Apply(new ProfilePhotoUpdated
+        {
+            UserId = Id,
+            PhotoUrl = photoUrl.ToString()
+        });
+
     protected override void When(IEvent eventHappened)
     {
-
+        var @event = eventHappened as UserEvent;
+        Action when = @event switch
+        {
+            ProfilePhotoUpdated e    => () => { },
+            UserDisplayNameUpdated e => () => { },
+            UserFullNameUpdated e    => () => { },
+            UserRegistered e         => () => { },
+            _                        => throw new ArgumentOutOfRangeException(nameof(@event))
+        };
     }
 
     protected override void EnsureValidState()
