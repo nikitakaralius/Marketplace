4813cc61e2bb6d9c19d9659cd0e7d71c0a2bbe45
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-13 17:54:17 +0300

    Replaced ClassifiedAds api with controller

--- src/Marketplace.Api/ClassifiedAds/CommandsApi.cs
+++ src/Marketplace.Api/ClassifiedAds/CommandsApi.cs
@@ -1,27 +1,38 @@
 using Marketplace.Infrastructure.Common;
+using Microsoft.AspNetCore.Mvc;
 using static Marketplace.ClassifiedAds.ClassifiedAdContract;
 
 namespace Marketplace.ClassifiedAds;
 
-using Service = ClassifiedAdsApplicationService;
-
-public static class CommandsApi
+[ApiController, Route("ad")]
+public class CommandsApi : ControllerBase
 {
-    public static void MapClassifiedAdsCommandsApi(this IEndpointRouteBuilder app)
+    private readonly IRequestHandler _handler;
+    private readonly ClassifiedAdsApplicationService _service;
+
+    public CommandsApi(IRequestHandler handler, ClassifiedAdsApplicationService service)
     {
-        app.MapPost("ad", async (IRequestHandler handler, Service service, V1.Create request) =>
-                        await handler.HandleRequestAsync(request, service.HandleAsync));
-
-        app.MapPut("ad/title", async (IRequestHandler handler, Service service, V1.SetTitle request) =>
-                       await handler.HandleRequestAsync(request, service.HandleAsync));
-
-        app.MapPut("ad/description", async (IRequestHandler handler, Service service, V1.UpdateDescription request) =>
-                       await handler.HandleRequestAsync(request, service.HandleAsync));
-
-        app.MapPut("ad/price", async (IRequestHandler handler, Service service, V1.UpdatePrice request) =>
-                       await handler.HandleRequestAsync(request, service.HandleAsync));
-
-        app.MapPut("ad/publish", async (IRequestHandler handler, Service service, V1.RequestToPublish request) =>
-                       await handler.HandleRequestAsync(request, service.HandleAsync));
+        _handler = handler;
+        _service = service;
     }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(V1.Create request) =>
+        await _handler.HandleRequestAsync(request, _service.HandleAsync);
+
+    [HttpPut("title")]
+    public async Task<IActionResult> Put(V1.SetTitle request) =>
+        await _handler.HandleRequestAsync(request, _service.HandleAsync);
+
+    [HttpPut("description")]
+    public async Task<IActionResult> Put(V1.UpdateDescription request) =>
+        await _handler.HandleRequestAsync(request, _service.HandleAsync);
+
+    [HttpPut("price")]
+    public async Task<IActionResult> Put(V1.UpdatePrice request) =>
+        await _handler.HandleRequestAsync(request, _service.HandleAsync);
+
+    [HttpPut("publish")]
+    public async Task<IActionResult> Put(V1.RequestToPublish request) =>
+        await _handler.HandleRequestAsync(request, _service.HandleAsync);
 }
