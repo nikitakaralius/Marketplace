fa51f93b758b5f35b68544d37e8af15e07763ab2
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-14 15:37:55 +0300

    Implemented last queries

--- src/Marketplace.Api/ClassifiedAds/Queries.cs
+++ src/Marketplace.Api/ClassifiedAds/Queries.cs
@@ -19,11 +19,51 @@ internal static class Queries
         @PageSize OFFSET @Offset
         """;
 
-        var request =connection.QueryAsync<ClassifiedAdListItem>(sql, new
+        var request = connection.QueryAsync<ClassifiedAdListItem>(sql, new
         {
             State = (int) ClassifiedAd.AdState.Active,
             PageSize = query.PageSize,
             Offset = Offset(query.Page, query.PageSize)
+        });
+
+        return await request;
+    }
+
+    public static async Task<IEnumerable<ClassifiedAdListItem>> QueryAsync(
+        this DbConnection connection,
+        GetOwnersClassifiedAds query)
+    {
+        const string sql = """
+        SELECT "DatabaseId", "Price_Amount" price, "Title_Value" title
+        FROM "ClassifiedAds"
+        WHERE "OwnerId_Value"=@OwnerId
+        LIMIT @PageSize
+        OFFSET @Offset
+        """;
+
+        var request = connection.QueryAsync<ClassifiedAdListItem>(sql, new
+        {
+            OwnerId = query.OwnerId,
+            PageSize = query.PageSize,
+            Offset = Offset(query.Page, query.PageSize)
+        });
+
+        return await request;
+    }
+
+    public static async Task<ClassifiedAdDetails?> QueryAsync(
+        this DbConnection connection,
+        GetPublicClassifiedAd query)
+    {
+        const string sql = """
+        SELECT "ClassifiedAds.DatabaseId" adId, "PriceAmount", "Title_Value", "Description_Value", "Display_Name"
+        FROM "ClassifiedAds", "UserProfiles"
+        WHERE adId=@Id AND "OwnerId_Value"="UserProfiles.DatabaseId"
+        """;
+
+        var request = connection.QuerySingleOrDefaultAsync<ClassifiedAdDetails>(sql, new
+        {
+            Id = query.ClassifiedAdId
         });
 
         return await request;
