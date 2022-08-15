d5ad3a128bc8c00631dba9a02377e0670b4daf81
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-15 11:18:31 +0300

    Added Version prop to AggregateRoot

--- src/Marketplace.Framework/AggregateRoot.cs
+++ src/Marketplace.Framework/AggregateRoot.cs
@@ -6,9 +6,20 @@ public abstract class AggregateRoot<TId> : IInternalEventHandler
 
     public TId Id { get; protected set; }
 
-    public void ClearChanges() => _changes.Clear();
+    public int Version { get; private set; } = -1;
 
     public IEnumerable<IEvent> Changes() => _changes;
+
+    public void ClearChanges() => _changes.Clear();
+
+    public void Load(IEnumerable<IEvent> history)
+    {
+        foreach (var e in history)
+        {
+            When(e);
+            Version++;
+        }
+    }
 
     protected abstract void When(IEvent eventHappened);
 
