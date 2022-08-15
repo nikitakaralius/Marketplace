1a0ca00077b0177b96c2e18fa403543f1d00596c
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-15 11:13:14 +0300

    Implemeted SaveAsync method

--- src/Marketplace.Api/Infrastructure/Store/EsAggregateStore.cs
+++ src/Marketplace.Api/Infrastructure/Store/EsAggregateStore.cs
@@ -1,17 +1,30 @@
 using System.Text.Json;
+using EventStore.ClientAPI;
 
 namespace Marketplace.Infrastructure.Store;
 
 internal sealed class EsAggregateStore : IAggregateStore
 {
+    private readonly IEventStoreConnection _connection;
+
+    public EsAggregateStore(IEventStoreConnection connection) => _connection = connection;
+
     public Task<bool> ExistsAsync<TAggregate, TId>(TId aggregateId) where TAggregate : AggregateRoot<TId>
     {
         throw new NotImplementedException();
     }
 
-    public Task SaveAsync<TAggregate, TId>(TAggregate aggregate) where TAggregate : AggregateRoot<TId>
+    public async Task SaveAsync<TAggregate, TId>(TAggregate aggregate) where TAggregate : AggregateRoot<TId>
+                                                                       where TId : notnull
     {
-        throw new NotImplementedException();
+        var changes = aggregate.Changes()
+                               .Select(e => ConvertToEventData(e))
+                               .ToArray();
+
+        if (changes.Length == 0) return;
+
+        string stream = StreamName<TAggregate, TId>(aggregate);
+        await _connection.AppendToStreamAsync(stream, aggregate.Version, changes);
     }
 
     public Task<TAggregate> LoadAsync<TAggregate, TId>(TId aggregateId) where TAggregate : AggregateRoot<TId>
@@ -29,4 +42,19 @@ internal sealed class EsAggregateStore : IAggregateStore
 
     private static byte[] Serialize(object data) =>
         JsonSerializer.SerializeToUtf8Bytes(data);
+
+    private static EventData ConvertToEventData(IEvent e) =>
+        new(eventId: Guid.NewGuid(),
+            type: e.GetType().Name,
+            isJson: true,
+            data: Serialize(e),
+            metadata: Serialize(new EventMetadata
+            {
+                ClrType = e.GetType().AssemblyQualifiedName!
+            }));
+
+    private class EventMetadata
+    {
+        public string ClrType { get; init; } = "";
+    }
 }
