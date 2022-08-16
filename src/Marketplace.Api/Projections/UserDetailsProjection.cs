c420834fdb4c878b0f9c7838e65bd2084cf0dfae
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-16 11:55:40 +0300

    Created UserDetailsProjection

---
+++ src/Marketplace.Api/Projections/UserDetailsProjection.cs
@@ -0,0 +1,31 @@
+using Marketplace.Domain.UserProfile;
+
+namespace Marketplace.Projections;
+
+internal sealed class UserDetailsProjection : IProjection
+{
+    private readonly IList<ReadModels.UserDetails> _items;
+
+    public UserDetailsProjection(IList<ReadModels.UserDetails> items) => _items = items;
+
+    public Task ProjectAsync(IEvent @event)
+    {
+        Action projection = @event switch
+        {
+            Events.UserRegistered e => () =>
+                _items.Add(new ReadModels.UserDetails {Id = e.UserId, DisplayName = e.DisplayName}),
+            Events.UserDisplayNameUpdated e => () =>
+                UpdateItem(e.UserId, x => x.DisplayName = e.DisplayName),
+            _ => () => { }
+        };
+        projection();
+        return Task.CompletedTask;
+    }
+
+    private void UpdateItem(Guid id, Action<ReadModels.UserDetails> update)
+    {
+        var item = _items.FirstOrDefault(x => x.Id == id);
+        if (item is null) return;
+        update(item);
+    }
+}
