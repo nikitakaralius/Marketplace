fce9f67bda0caadbbeed70220034b02f23ec4a1e
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-16 11:35:29 +0300

    Created ClassifiedAdDetails Projection

---
+++ src/Marketplace.Api/Projections/ClassifiedAdDetailsProjection.cs
@@ -0,0 +1,51 @@
+using Marketplace.Domain.ClassifiedAd;
+
+namespace Marketplace.Projections;
+
+using AdEvent = Marketplace.Domain.ClassifiedAd.Events;
+using UserEvent = Marketplace.Domain.UserProfile.Events;
+
+internal sealed class ClassifiedAdDetailsProjection : IProjection
+{
+    private readonly IList<ReadModels.ClassifiedAdDetails> _items;
+
+    public ClassifiedAdDetailsProjection(IList<ReadModels.ClassifiedAdDetails> items) => _items = items;
+
+    public Task ProjectAsync(IEvent @event)
+    {
+        Action project = @event switch
+        {
+            AdEvent.ClassifiedAdCreated e => () =>
+                _items.Add(new ReadModels.ClassifiedAdDetails {Id = e.Id, SellerId = e.Id}),
+            AdEvent.ClassifiedAdDescriptionUpdated e => () =>
+                UpdateItem(e.Id, x => x.Description = e.Description),
+            AdEvent.ClassifiedAdPriceUpdated e => () =>
+                UpdateItem(e.Id, x => (x.Price, x.CurrencyCode) = (e.Price, e.CurrencyCode)),
+            AdEvent.ClassifiedAdTitleChanged e => () =>
+                UpdateItem(e.Id, x => x.Title = e.Title),
+            UserEvent.UserDisplayNameUpdated e => () =>
+                UpdateMultipleItems(
+                    q => q.SellerId == e.UserId,
+                    ad => ad.SellerDisplayName = e.DisplayName),
+            _ => () => { }
+        };
+
+        project();
+
+        return Task.CompletedTask;
+    }
+
+    private void UpdateItem(Guid id, Action<ReadModels.ClassifiedAdDetails> update)
+    {
+        var item = _items.FirstOrDefault(x => x.Id == id);
+        if (item is null) return;
+        update(item);
+    }
+
+    private void UpdateMultipleItems(Func<ReadModels.ClassifiedAdDetails, bool> query,
+                                     Action<ReadModels.ClassifiedAdDetails> update)
+    {
+        foreach (var item in _items.Where(query))
+            update(item);
+    }
+}
