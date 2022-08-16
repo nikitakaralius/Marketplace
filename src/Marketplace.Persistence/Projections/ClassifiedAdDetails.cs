301b85fc499fee75c368eaf178da12dcc20367d5
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-16 19:10:54 +0300

    Created projections

---
+++ src/Marketplace.Persistence/Projections/ClassifiedAdDetails.cs
@@ -0,0 +1,20 @@
+namespace Marketplace.Persistence.Projections;
+
+public sealed class ClassifiedAdDetails
+{
+    public Guid Id { get; init; }
+
+    public string Title { get; set; } = "";
+
+    public string Description { get; set; } = "";
+
+    public decimal Price { get; set; }
+
+    public string CurrencyCode { get; set; } = "";
+
+    public string[] PhotoUrls { get; set; } = Array.Empty<string>();
+
+    public Guid SellerId { get; init; }
+
+    public UserDetails Seller { get; init; } = new();
+}
