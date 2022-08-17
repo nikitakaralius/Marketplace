648596ba1cdf265e3d6d1f7332e7767caf39422d
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-17 10:27:29 +0300

    Implemented ClassifiedAdRepository

---
+++ src/Marketplace.EntityFramework/Concrete/ClassifiedAdRepository.cs
@@ -0,0 +1,17 @@
+using Marketplace.Infrastructure.Persistence;
+
+namespace Marketplace.EntityFramework.Concrete;
+
+internal sealed class ClassifiedAdRepository : IClassifiedAdRepository
+{
+    private readonly MarketplaceDbContext _context;
+
+    public ClassifiedAdRepository(MarketplaceDbContext context) =>
+        _context = context;
+
+    public async Task AddAsync(ClassifiedAdDetails ad) =>
+        await _context.ClassifiedAds.AddAsync(ad);
+
+    public async Task<ClassifiedAdDetails?> ByIdAsync(Guid id) =>
+        await _context.ClassifiedAds.FindAsync(id);
+}
