188a01150a9b29d8f6b602a144fa662cdbf6d230
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-17 10:18:54 +0300

    Referenced Infrastructure project

--- src/Marketplace.EntityFramework/GlobalUsings.cs
+++ src/Marketplace.EntityFramework/GlobalUsings.cs
@@ -1,6 +1,6 @@
 // Global using directives
 
 global using Marketplace.ClassifiedAds.Projections;
-global using Marketplace.EntityFramework.Checkpoints;
+global using Marketplace.Infrastructure.Checkpoints;
 global using Marketplace.Users.Projections;
 global using Microsoft.EntityFrameworkCore;
